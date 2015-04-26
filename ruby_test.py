010749029e367a22401b7eded4a78676d7fa75d7
Robert D. French <robert@robertdfrench.me>
2015-04-26 16:59:10 -0400

    Use python2 for now

--- ruby_test.py
+++ ruby_test.py
@@ -1,11 +1,11 @@
 import os
 
 def snippet_exists_test():
-	os.system("python3 regional.py example.rb")
+	os.system("python regional.py example.rb")
 	assert(os.path.isfile("ReopenExistingClass.snippet.rb"))
 
 def extracts_correct_region_test():
-	os.system("python3 regional.py example.rb")
+	os.system("python regional.py example.rb")
 	expected_contents = "class Doofus\n\tdef self.greet_differently\n\t\tgreet\n\t\tputs \"... my friend\"\n\tend\nend\n"
 	with open("ReopenExistingClass.snippet.rb") as f:
 		actual_contents = "".join(f.readlines())
